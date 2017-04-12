cd67d943ac0909634cf5623e621dded3091fc6ae
Jaroslav Budis <jaroslav.budis@gmail.com>
2017-04-12 14:23:28 +0200

    FastQC summaries may now be generated from subset of files

--- preprocess/report/scripts/fastqc-summary.py
+++ preprocess/report/scripts/fastqc-summary.py
@@ -1,37 +1,44 @@
 #!/usr/bin/python
 
+from argparse import ArgumentParser
+import os
+
 fastqc_suffix = '_fastqc.html'
 
-from argparse import ArgumentParser
-parser = ArgumentParser(description="Takes all fastqc reports in the specified directory and generated one page summarized report")
-parser.add_argument('report_dir', help="Directory with fastqc files (files with %s suffix)"%fastqc_suffix)
+parser = ArgumentParser(
+    description="Takes all fastqc reports in the specified directory and generated one page summarized report")
 parser.add_argument('summary_file', help="Output html file with summarized fastq files")
+parser.add_argument('reports', nargs="*", help="Fastqc files (files with %s suffix)" % fastqc_suffix)
 args = parser.parse_args()
 
-report_dir, out_file = args.report_dir, args.summary_file
+report_files, out_file = sorted(args.reports), args.summary_file
 
-from os import listdir
 from xml.dom import minidom
 
-report_files = {report_file[:-len(fastqc_suffix)]: '%s/%s'%(report_dir, report_file) for report_file in listdir(report_dir) if report_file.endswith(fastqc_suffix)}
-report_names = sorted(report_files.keys())
-reports = map(lambda report_name: minidom.parse(open(report_files[report_name])), report_names)
+report_names = [os.path.basename(rf)[:-len(fastqc_suffix)] for rf in report_files]
+reports = [minidom.parse(open(rf)) for rf in report_files]
+
 
 def retrieve_headers(report):
-	return report.getElementsByTagName('h2')
+    return report.getElementsByTagName('h2')
+
 
 def retrieve_icons(report):
-	h2s = retrieve_headers(report)[1:]
-	return map(lambda x: x.getElementsByTagName('img')[0], h2s)
+    h2s = retrieve_headers(report)[1:]
+    return map(lambda x: x.getElementsByTagName('img')[0], h2s)
+
 
 def retrieve_labels(report):
-	h2s = retrieve_headers(report)[1:]
-	return map(lambda x: x.lastChild.toxml(), h2s)
+    h2s = retrieve_headers(report)[1:]
+    return map(lambda x: x.lastChild.toxml(), h2s)
+
 
 def retrieve_summary(report):
-	stat_table = report.getElementsByTagName('table')[0].childNodes[1]
-	sequences_row, lens_row = 3, 5
-	return map(lambda row_id: stat_table.childNodes[row_id].childNodes[1].firstChild.nodeValue, [sequences_row, lens_row])
+    stat_table = report.getElementsByTagName('table')[0].childNodes[1]
+    sequences_row, lens_row = 3, 5
+    return map(lambda row_id: stat_table.childNodes[row_id].childNodes[1].firstChild.nodeValue,
+               [sequences_row, lens_row])
+
 
 icons = map(retrieve_icons, reports)
 labels = retrieve_labels(reports[0])
@@ -43,28 +50,34 @@ html.appendChild(head)
 html.appendChild(body)
 body.appendChild(table)
 
+
 def prepare_text_cell(text):
-	td = doc.createElement('td')
-	td.attributes['style'] = 'padding-left: 20px; padding-right: 20px'
-	td.appendChild(doc.createTextNode(text))
-	return td
+    td = doc.createElement('td')
+    td.attributes['style'] = 'padding-left: 20px; padding-right: 20px'
+    td.appendChild(doc.createTextNode(text))
+    return td
 
-def generate_icon_row(report_name, report_icons, report):
-	tr = doc.createElement('tr')
-	sequences, lens = retrieve_summary(report)
-	for text in [report_name, '{0:,}'.format(int(sequences)), lens]:
-		tr.appendChild(prepare_text_cell(text))
-	for icon, pos in zip(report_icons, xrange(len(report_icons))):
-		td, a = doc.createElement('td'), doc.createElement('a')
-		a.attributes['href'] = '%s#M%s'%(report_files[report_name], pos)
-		a.attributes['title'] = labels[pos]
-		a.appendChild(icon)
-		td.appendChild(a)
-		tr.appendChild(td)
-	return tr
 
-rows = map(lambda (report_name, report_icons, report): generate_icon_row(report_name, report_icons, report) ,zip(report_names, icons, reports))
+def generate_icon_row(report_name, report_file, report_icons, report):
+    tr = doc.createElement('tr')
+    sequences, lens = retrieve_summary(report)
+    for text in [report_name, '{0:,}'.format(int(sequences)), lens]:
+        tr.appendChild(prepare_text_cell(text))
+    for icon, pos in zip(report_icons, xrange(len(report_icons))):
+        td, a = doc.createElement('td'), doc.createElement('a')
+        a.attributes['href'] = '%s#M%s' % (report_file.split('/')[-1], pos)
+        a.attributes['title'] = labels[pos]
+        a.appendChild(icon)
+        td.appendChild(a)
+        tr.appendChild(td)
+    return tr
+
+
+rows = []
+for (report_name, report_file, report_icons, report) in zip(report_names, report_files, icons, reports):
+    rows.append(generate_icon_row(report_name, report_file, report_icons, report))
+
 for row in rows:
-	table.appendChild(row)
+    table.appendChild(row)
 
 doc.writexml(open(out_file, 'w'))
