3e7099992f87211518e608abd0dffcd64fdfca39
Jaroslav Budis <jaroslav.budis@gmail.com>
2018-03-09 08:06:25 +0100

    Q value cannot be calculated for phage cocktails, omitting

--- classification/viral/virfinder/virfinder_wrapper.R
+++ classification/viral/virfinder/virfinder_wrapper.R
@@ -6,5 +6,5 @@ fasta <- commandArgs(TRUE)[1]
 out <- commandArgs(TRUE)[2]
 
 predResult <- VF.pred(fasta)
-predResult$qvalue <- VF.qvalue(predResult$pvalue)
+# predResult$qvalue <- VF.qvalue(predResult$pvalue)
 write.table(predResult, out, sep='\t', row.names=F, quote=F)
