be4ebaa63fe66081d58b4343369e8d6f10bcc50f
hekel <rastislav.hekel@geneton.sk>
2019-05-15 16:14:04 +0200

    genomic panels implemented
    dbsnp/dbsnp.vcf.gz -> variants/dbsnp.vcf.bgz
    .vcf.gz -> .vcf.bgz
    .vcf.bgz.tbi. -> .vcf.bgz.tbi
    
    qualimap qc report panel case fixed

--- example/genomic/reference/mhv/readme.txt
+++ example/genomic/reference/mhv/readme.txt
@@ -1,6 +1,6 @@
 mhv.fa
 ***************
-two positions of the reference genome were altered to create two virtual variants
+the reference genome was altered in two positions to create two virtual variants
 8681 (C->T)
 11201 (T->A)
 
--- src/pipeline/samples.py
+++ src/pipeline/samples.py
@@ -7,15 +7,15 @@ class Pipeline:
     Stores information of samples, their references and targeted panels in structures suitable
     for SnakeMake expand() function
     """
-
+    WGS_PANEL = 'wgs'
+    
     def __init__(self):
         self.samples = []
         self.references = []
         self.panels = []
         self.sample_references = []
-        self.sample_defs = []
         self.__reference_map = {}
-
+    
     def add(self, samples, reference, panel, prebuilt_reference):
         """
         Add samples intended for analysis.
@@ -25,61 +25,60 @@ class Pipeline:
         :param prebuilt_reference: if reference is only a parameter to a specific tool that utilize its own database,
                                    and so reference/{reference}/{reference}.fa does not exist
         """
-
+        
         # Check if both R1 and R2 files for all samples exist
         for sample in samples:
             r1, r2 = 'reads/original/{}_R1.fastq.gz'.format(sample), 'reads/original/{}_R2.fastq.gz'.format(sample)
             assert os.path.exists(r1), 'Read file {} does not exist'.format(r1)
             assert os.path.exists(r2), 'Read file {} does not exist'.format(r2)
-
+        
         # Check, if reference file exists
         if reference and not prebuilt_reference:
             fasta = 'reference/{reference}/{reference}.fa'.format(reference=reference)
             assert os.path.exists(fasta), 'Reference fasta {} does not exist'.format(fasta)
-
+        
         # Check, if panel bed file exists
         if panel:
             assert reference, 'Panel cannot be defined without reference'
-            bed = 'references/{reference}/{reference}/annotation/{panel}/regions.bed'.format(reference=reference, panel=panel)
+            bed = 'reference/{reference}/annotation/{panel}/regions.bed'.format(reference=reference, panel=panel)
             assert os.path.exists(bed), 'Panel bed file {} does not exist'.format(bed)
-
+        
         # Extend lists
         self.samples.extend(samples)
-
+        
+        if panel:
+            if panel not in self.panels:
+                self.panels.append(panel)
+        else:
+            panel = self.WGS_PANEL
+        
         if reference:
-
             if reference not in self.references:
                 self.references.append(reference)
-
+            
             if reference in self.__reference_map:
                 self.__reference_map[reference].extend(samples)
             else:
                 self.__reference_map[reference] = samples
-
+            
             for sample in samples:
                 # TODO here should be check if tuple already stored
-                self.sample_references.append(namedtuple('SampleReference', 'sample reference')(sample, reference))
-
-        if panel:
-            if panel not in self.panels:
-                self.panels.append(panel)
-            for sample in samples:
-                # TODO here should be check if tuple already stored
-                self.sample_defs.append(namedtuple('SampleDef', 'sample reference panel')(sample, reference, panel))
-
+                SampleReference = namedtuple('SampleReference', 'sample reference panel')
+                self.sample_references.append(SampleReference(sample, reference, panel))
+    
     def is_empty(self):
         """
         Has at least one sample to be analysed?
         """
         return len(self.samples) == 0
-
+    
     def samples_for(self, reference):
         """
         Samples that should be analysed with defined reference
         :param reference: name of the reference
         """
         return self.__reference_map[reference]
-
+    
     def __str__(self):
         """
         Returns the string representation of this class
