114bddb6dadad579706651cd1f76c2d388019a33
hekel <rastislav.hekel@geneton.sk>
2021-03-22 19:14:23 +0100

    configurable coverage for masking consensus fasta

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = u'Jaroslav Budis'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.11.2'
+release = u'0.11.3'
 
 
 # -- General configuration ---------------------------------------------------
