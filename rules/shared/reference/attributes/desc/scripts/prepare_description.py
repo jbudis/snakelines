205ad667cc9271334944bedd8fb3b73c7b02c5b8
Gogis0 <adriangoga0@gmail.com>
2021-12-08 11:14:58 +0100

    Fix /reference/attributes/desc/custom.snake
    TODO: Fix the description in dependency_shared.yaml

--- rules/shared/reference/attributes/desc/scripts/prepare_description.py
+++ rules/shared/reference/attributes/desc/scripts/prepare_description.py
@@ -1,8 +1,8 @@
 import sys
 from Bio import SeqIO
 
-input_reference = sys.argv[2]
-output_dict = sys.argv[3]
+input_reference = sys.argv[1]
+output_dict = sys.argv[2]
 
 
 with open(output_dict, 'w') as out:
