10f9f51bdfd3c48ca96e3e6e9e42cda872b1012f
jsitarcik <xsitarcik@github.com>
2022-10-05 09:48:43 +0200

    added updating freyja

---
+++ rules/shared/mixture_composition/caller/freyja_update.sh
@@ -0,0 +1,3 @@
+eval "$(conda shell.bash hook)"
+conda activate freyja-env
+freyja update --outdir /home_pfs/data/narcos/production/scripts/snakelines/dev/rules/shared/mixture_composition/caller/lineages
