5ab563f809f5eba57139ef7d2691ee7d60f7d933
sitarcik <jozef.sitarcik@geneton.sk>
2023-01-12 21:20:42 +0100

    fixed qiime2_post_taxa pipeline attribute

--- src/config/impute_process_types.py
+++ src/config/impute_process_types.py
@@ -8,20 +8,20 @@ def impute_read_types(pipeline, config):
     """
     # Impute read types into the read preprocess steps
     original_type = 'original'
-    
+
     # TODO preprocessed should be called processed to unite with mapping
     setattr(pipeline, 'preprocessed_read_type', original_type)
-    
+
     # Find configuration file with reads->preprocess attributes
     if 'preprocess' not in config.get('reads', {}):
         return
-    
+
     # Add new read_type attribute to the main config, so rules for preprocess steps may deduce input read files
     previous_type = original_type
     for this_type in config['reads']['preprocess'].keys():
         config['reads']['preprocess'][this_type]['input_read_type'] = previous_type
         previous_type = this_type
-    
+
     setattr(pipeline, 'preprocessed_read_type', previous_type)
 
 
@@ -35,17 +35,17 @@ def impute_mapping_types(pipeline, config):
     """
     original_type = 'original'
     setattr(pipeline, 'postprocessed_map_type', original_type)
-    
+
     # Find configuration file with reads->preprocess attributes
     if 'postprocess' not in config.get('mapping', {}):
         return
-    
+
     # Add new read_type attribute to the main config, so rules for preprocess steps may deduce input read files
     previous_type = original_type
     for this_type in config['mapping']['postprocess'].keys():
         config['mapping']['postprocess'][this_type]['input_map_type'] = previous_type
         previous_type = this_type
-    
+
     setattr(pipeline, 'postprocessed_map_type', previous_type)
 
 
@@ -59,18 +59,18 @@ def impute_variant_types(pipeline, config):
     """
     original_type = 'original'
     setattr(pipeline, 'postprocessed_variant_type', original_type)
-    
+
     # Find configuration file with reads->preprocess attributes
     if 'postprocess' not in config.get('variant', {}):
         return
-    
+
     # Add new read_type attribute to the main config, so rules for preprocess steps may deduce input read files
     previous_type = original_type
     if config['variant']['postprocess'] is not None:
         for this_type in config['variant']['postprocess'].keys():
             config['variant']['postprocess'][this_type]['input_variant_type'] = previous_type
             previous_type = this_type
-    
+
     setattr(pipeline, 'postprocessed_variant_type', previous_type)
 
 
@@ -87,21 +87,21 @@ def impute_dada2_types(pipeline, config):
     setattr(pipeline, 'qiime2_post_taxa', original_type)
 
     # Find configuration file with reads->preprocess attributes
+    previous_type = None
     if 'preprocess' not in config.get('qiime2', {}):
         return
-    elif 'post_filter' not in config['qiime2']['preprocess']:
-        return
+    elif 'post_filter' in config['qiime2']['preprocess']:
 
-    # Add new read_type attribute to the main config, so rules for preprocess steps may deduce input read files
-    previous_type = original_type
-    if config['qiime2']['preprocess']['post_filter'] is not None:
-        for this_type in config['qiime2']['preprocess']['post_filter'].keys():
-            config['qiime2']['preprocess']['post_filter'][this_type]['input_qiime2_type'] = previous_type
-            previous_type = this_type
+        # Add new read_type attribute to the main config, so rules for preprocess steps may deduce input read files
+        previous_type = original_type
+        if config['qiime2']['preprocess']['post_filter'] is not None:
+            for this_type in config['qiime2']['preprocess']['post_filter'].keys():
+                config['qiime2']['preprocess']['post_filter'][this_type]['input_qiime2_type'] = previous_type
+                previous_type = this_type
 
-    setattr(pipeline, 'qiime2_type', previous_type)
+        setattr(pipeline, 'qiime2_type', previous_type)
 
     if 'analysis' in config['qiime2'] and 'taxa_filter' in config['qiime2']['analysis']:
         setattr(pipeline, 'qiime2_post_taxa', 'taxa_filter')
-    else:
+    elif previous_type:
         setattr(pipeline, 'qiime2_post_taxa', previous_type)
