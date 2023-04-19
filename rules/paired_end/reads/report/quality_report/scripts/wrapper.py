249bcb096bae31bb104a464a67b6ff59c7b56cc8
xsitarcik <jozefsitarcik@gmail.com>
2023-04-19 12:30:04 +0200

    removed auxiliary output

--- rules/paired_end/reads/report/quality_report/scripts/wrapper.py
+++ rules/paired_end/reads/report/quality_report/scripts/wrapper.py
@@ -21,14 +21,12 @@ with tempfile.TemporaryDirectory() as tmpdir:
         raise ValueError(f"{snakemake.input.reads} does not ends with {PRESUMED_SUFFIX}")
 
     base_name = os.path.basename(snakemake.input.reads).replace(".fastq.gz", "")
-    print("basename",base_name)
     html_path = os.path.join(tmpdir, f"{base_name}_fastqc.html")
     zip_path = os.path.join(tmpdir, f"{base_name}_fastqc.zip")
 
     fastqc_datapath = os.path.join(tmpdir, f"{base_name}_fastqc", "fastqc_data.txt")
     summary_path = os.path.join(tmpdir, f"{base_name}_fastqc", "summary.txt")
 
-    shell("ls {tmpdir}")
     shell("mv {html_path} {snakemake.output.html}")
     shell("mv {zip_path} {snakemake.output.zip}")
     shell("mv {fastqc_datapath} {snakemake.output.data}")
