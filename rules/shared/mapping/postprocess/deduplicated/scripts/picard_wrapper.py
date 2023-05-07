074020f9dfecbb8f579f7000b61f861f4f3c279d
xsitarcik <jozefsitarcik@gmail.com>
2023-05-07 18:29:10 +0200

    fix: picard using temp dir with context manager

---
+++ rules/shared/mapping/postprocess/deduplicated/scripts/picard_wrapper.py
@@ -0,0 +1,18 @@
+import tempfile
+from snakemake.shell import shell
+
+
+sys.stderr = open(snakemake.log.err, "w")
+sys.stdout = open(snakemake.log.out, "w")
+
+
+with tempfile.TemporaryDirectory() as tmpdir:
+    shell(
+        "picard MarkDuplicates"
+        " I={snakemake.input.bam}"
+        " O={snakemake.output.bam}"
+        " M={snakemake.log.stat}"
+        " TMP_DIR={tmpdir}"
+        " VALIDATION_STRINGENCY=SILENT"
+    )
+
